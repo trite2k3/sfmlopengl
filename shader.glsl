09d0346007f1eee357d27819872b6c2a25648662
Trite2k3 <mftrite@gmail.com>
2024-05-17 17:35:29 +0200

    copy paste shaders :D

--- shader.glsl
+++ shader.glsl
@@ -1,17 +1,51 @@
 #shader vertex
 #version 330 core
-layout(location = 0) in vec2 position;
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+
+out vec3 fragPosition;
+out vec3 fragNormal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(position, 0.0, 1.0);
-}
+    fragPosition = vec3(view * model * vec4(position, 1.0));
+    fragNormal = mat3(transpose(inverse(view * model))) * normal;
 
+    gl_Position = projection * view * model * vec4(position, 1.0);
+}
 
 #shader fragment
 #version 330 core
+
+in vec3 fragPosition;
+in vec3 fragNormal;
+
 out vec4 outColor;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
 void main()
 {
-    outColor = vec4(0.3, 0.8, 0.0, 1.0);
-}
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * vec3(1.0, 1.0, 1.0);
 
+    vec3 norm = normalize(fragNormal);
+    vec3 lightDir = normalize(lightPos - fragPosition);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * vec3(1.0, 1.0, 1.0);
+
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - fragPosition);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * vec3(1.0, 1.0, 1.0);
+
+    vec3 result = ambient + diffuse + specular;
+    outColor = vec4(result, 1.0);
+}
