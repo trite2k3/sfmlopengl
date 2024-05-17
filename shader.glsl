3c6d827cb840e714d51f1d0b2e4f4316f92266d0
Trite2k3 <mftrite@gmail.com>
2024-05-17 19:13:19 +0200

    ball resizing

--- shader.glsl
+++ shader.glsl
@@ -1,11 +1,11 @@
 #shader vertex
-#version 330 core
+#version 460 core
 
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
 
-out vec3 fragPosition;
-out vec3 fragNormal;
+out vec3 FragPos;
+out vec3 Normal;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -13,39 +13,42 @@ uniform mat4 projection;
 
 void main()
 {
-    fragPosition = vec3(view * model * vec4(position, 1.0));
-    fragNormal = mat3(transpose(inverse(view * model))) * normal;
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(model))) * aNormal;
 
-    gl_Position = projection * view * model * vec4(position, 1.0);
+    gl_Position = projection * view * vec4(FragPos, 1.0);
 }
 
 #shader fragment
-#version 330 core
+#version 460 core
 
-in vec3 fragPosition;
-in vec3 fragNormal;
+out vec4 FragColor;
 
-out vec4 outColor;
+in vec3 FragPos;
+in vec3 Normal;
 
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 void main()
 {
+    // Ambient
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * vec3(1.0, 1.0, 1.0);
 
-    vec3 norm = normalize(fragNormal);
-    vec3 lightDir = normalize(lightPos - fragPosition);
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * vec3(1.0, 1.0, 1.0);
 
+    // Specular
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - fragPosition);
+    vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * vec3(1.0, 1.0, 1.0);
 
-    vec3 result = ambient + diffuse + specular;
-    outColor = vec4(result, 1.0);
+    vec3 result = (ambient + diffuse + specular) * vec3(1.0, 0.5, 0.31);
+    FragColor = vec4(result, 1.0);
 }
